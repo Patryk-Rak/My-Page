c69ca27464b81c992d1f4c0c804347788cdd5854
Dan-Timefly <dan.timefly@gmail.com>
2019-05-01 21:34:51 +0200

    Added card-box

--- assets/navbar.js
+++ assets/navbar.js
@@ -1,10 +1,9 @@
-$(window).on("scroll", function() {
-             if($(window).scrollTop()) {
-                $('nav').addClass('full-white-on-scroll');
-             }
-             else {
-                 $('nav').removeClass('full-white-on-scroll');
-             }
+$(window).on("scroll", function () {
+    if ($(window).scrollTop()) {
+        $('nav').addClass('full-white-on-scroll');
+    } else {
+        $('nav').removeClass('full-white-on-scroll');
+    }
 });
 
 
@@ -14,26 +13,25 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
-    
 
-    burger.addEventListener('click',()=> {
+
+    burger.addEventListener('click', () => {
         //Toggle Nav
         nav.classList.toggle('nav-active');
-        
+
         //Animate Links
         navLinks.forEach((link, index) => {
-            if(link.style.animation) {
+            if (link.style.animation) {
                 link.style.animation = '';
-            }
-            else {
+            } else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 10 + 0.5}s`;
             }
         });
-        
+
         //Burger Animation
         burger.classList.toggle('toggle');
-        
-    });   
+
+    });
 }
 
 navSlide();
